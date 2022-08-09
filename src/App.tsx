575db985421b5450e6f7138438bf4607a74054a5
wangshangzhe <wangshanghzhe@didiglobal.com>
2022-08-09 21:13:49 +0800

    feat: 移除严格模式对effect的影响

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import './App.css';
 import Home from './page/home'
 
 function App() {
-  const curTimeStamp = new Date().getMilliseconds();;
+  const curTimeStamp = new Date().getTime();
   const startTime = '2022-08-09 10:00:00';
   const endTime = '2022-08-09 22:00:00';
+  console.log('curTimeStamp in App==', curTimeStamp)
   return (
     <div className="App">
       <header className="App-header">
--- src/page/home.tsx
+++ src/page/home.tsx
@@ -6,23 +6,24 @@ const Home: React.FC<{
   endTime:string,
   curTimeStamp: number
 }> = (props) => {
-  const {curTimeStamp, startTime, endTime} = props
-  const endTimeStamp = dayjs(endTime).millisecond()
-  const [curTime, setCurTime] = useState('')
-  const [remainStamp, setRemainStamp] = useState(endTimeStamp - curTimeStamp)
+  const {curTimeStamp, startTime, endTime} = props;
+  const endTimeStamp = dayjs(endTime).millisecond();
+  const [curTime, setCurTime] = useState('');
+  console.log('curTime==', curTime);
+  const [remainStamp, setRemainStamp] = useState(endTimeStamp - curTimeStamp);
 
-  const endIntervalTimer = setInterval(() => {
-    const cur = dayjs(remainStamp).format('HH:mm:ss')
-    setRemainStamp(remainStamp-1000)
-    setCurTime(cur)
-  },1000)
+  const endTimeoutTimer = setTimeout(() => {
+    const cur = dayjs(remainStamp).format('HH:mm:ss');
+    setRemainStamp(remainStamp - 1000);
+    setCurTime(cur);
+  },1000);
 
   useEffect(() => {
     return componentWillUnmount;
-  }, []);
+  }, [curTime]);
 
   const componentWillUnmount = () => {
-    clearInterval(endIntervalTimer)
+    clearTimeout(endTimeoutTimer);
   }
 
   return <div>
